3435809e2a7aeda35c3d219468b0555989a44957
LeoCosta-Dev <leonardodacostadasilva@hotmail.com>
2021-09-03 12:52:50 -0300

    definição de script padrão inicial

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ const enviar = $('#enviar')
 
 // Eventos
 
+defaultsPage()
+
 enviar.on('click', () =>{
     nasa()
 })
 
 // Lógica de requisição de api e manipulação de documento com Jquery
 
-$.ajax({
-    url: `https://api.nasa.gov/planetary/apod?api_key=Zt5ss0RRWEJhhqmyjC3hqGiOwqGeAKxXR4yumBwm`,
-    method: 'get',
-    success: (response) => {
-        imagem.css('background-image',`url(${response.url})`)
-        $('#date').prop('max', response.date)
-        console.log(response)
-    }
-})
+function defaultsPage(){
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=Zt5ss0RRWEJhhqmyjC3hqGiOwqGeAKxXR4yumBwm`,
+        method: 'get',
+        success: (response) => {
+            imagem.css('background-image',`url(${response.url})`)
+            $('#date').prop('max', response.date)
+            $('#date').prop('value', response.date)
+            console.log(response)
+        }
+    })
+}
+
 
 function nasa(){
     
