0d31369d370a64e8bf623c590afdb743e4ec4f9d
LeoCosta-Dev <leonardodacostadasilva@hotmail.com>
2021-09-04 21:05:12 -0300

    tratamento para quando for video

--- script.js
+++ script.js
@@ -7,33 +7,49 @@ const enviar = $('#enviar')
 
 defaultsPage()
 
-enviar.on('click', () =>{
+enviar.on('click', () => {
     nasa()
 })
 
 // Lógica de requisição de api e manipulação de documento com Jquery
 
-function defaultsPage(){
+function defaultsPage() {
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=Zt5ss0RRWEJhhqmyjC3hqGiOwqGeAKxXR4yumBwm`,
         method: 'get',
         success: (response) => {
             $('#date').prop('max', response.date)
             $('#date').prop('value', response.date)
-            imagem.css('background-image',`url(${response.url})`)
             $('#texto').html(response.explanation)
+            if (response.media_type == 'image') {
+                imagem.css('background-image', `url(${response.url})`)
+                document.getElementById('imagem').style.display = 'flex'
+                document.getElementById('video').style.display = 'none'
+            } else if (response.media_type == 'video') {
+                video.setAttribute('src', response.url)
+                document.getElementById('imagem').style.display = "none"
+                document.getElementById('video').style.display = 'flex'
+            }
         }
     })
 }
 
 
-function nasa(){ 
+function nasa() {
     return $.ajax({
-       url: `https://api.nasa.gov/planetary/apod?api_key=Zt5ss0RRWEJhhqmyjC3hqGiOwqGeAKxXR4yumBwm&date=${date.value}`,
-       method: 'get',
-       success: (response) => {
-           imagem.css('background-image',`url(${response.url})`)
-           $('#texto').html(response.explanation)
-       }
+        url: `https://api.nasa.gov/planetary/apod?api_key=Zt5ss0RRWEJhhqmyjC3hqGiOwqGeAKxXR4yumBwm&date=${date.value}`,
+        method: 'get',
+        success: (response) => {
+            $('#texto').html(response.explanation)
+            if (response.media_type == 'image') {
+                imagem.css('background-image', `url(${response.url})`)
+                document.getElementById('imagem').style.display = 'flex'
+                document.getElementById('video').style.display = 'none'
+            } else if (response.media_type == 'video') {
+                video.setAttribute('src', response.url)
+                document.getElementById('imagem').style.display = "none"
+                document.getElementById('video').style.display = 'flex'
+            }
+        }
    })
 }
